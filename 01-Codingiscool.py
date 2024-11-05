6f3901aa7892a836a488c3788b80fab504f8aa95
luvkapoipoi <165130641+luvkapoipoi@users.noreply.github.com>
2024-11-05 20:21:06 +0400

    Exercise 1 done with explanations
    Just started the assessment today

---
+++ 01-Codingiscool.py
@@ -0,0 +1,15 @@
+#                  Exercise 1: Coding is Cool- 10 Marks
+# Fill in the blanks in the Python code below to output the phrase "Coding is Cool" 
+#         to the console using variables and string concatenation.
+
+#               word1 = 
+#               word2 = 
+#               word3 =
+
+word1 = ('Coding ')
+word2 = ('is ')
+word3 = ('Cool ')
+print (word1 + word2 + word3)
+
+
+#output = Coding is Cool
