1604c928b998b6b471c12e13651572e2ebfb052e
luvkapoipoi <165130641+luvkapoipoi@users.noreply.github.com>
2024-11-05 20:35:37 +0400

    Update 01-Codingiscool.py
    added an explanation

--- 01-Codingiscool.py
+++ 01-Codingiscool.py
@@ -1,15 +1,18 @@
-#                  Exercise 1: Coding is Cool- 10 Marks
-# Fill in the blanks in the Python code below to output the phrase "Coding is Cool" 
-#         to the console using variables and string concatenation.
+#                                 Exercise 1: Coding is Cool- 10 Marks
+#                Fill in the blanks in the Python code below to output the phrase "Coding is Cool" 
+#                        to the console using variables and string concatenation.
 
-#               word1 = 
-#               word2 = 
-#               word3 =
+#               word1 = _____
+#               word2 = _____
+#               word3 = _____
 
 word1 = ('Coding ')
 word2 = ('is ')
 word3 = ('Cool ')
-print (word1 + word2 + word3)
+print ( word1 + word2 + word3 )
+
+#Explanation: In this code, it sets word1 to "Coding ", word2 to "is ", and word3 to "Cool ". It then concatenates
+#             these variables with the + operators and prints the resulting string to the terminal. yes
 
 
 #output = Coding is Cool
