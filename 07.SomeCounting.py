6a507b8c481b07fe73db09b02bb80e674d1ba69b
luvkapoipoi <165130641+luvkapoipoi@users.noreply.github.com>
2024-11-16 22:59:18 +0400

    for loop trouble

--- 07.SomeCounting.py
+++ 07.SomeCounting.py
@@ -7,3 +7,16 @@
 # - Write a loop that counts up from 30 to 50 in increments of 1.
 # - Write a loop that counts down from 50 to 10 in decrements of 2.
 # - Write a loop that counts up from 100 to 200 in increments of 5. You may include all loops in a single project
+
+# Program
+
+# 0 to 50 in increments of 1
+print("Starting to count up from 0 to 50... ")
+for i in range(0, 51):
+    print (i)
+
+# 50 to 0 in decrements of 1
+print("Starting to count down from 50 to 0...")
+for i in range(51, 0):
+    print (i)
+
