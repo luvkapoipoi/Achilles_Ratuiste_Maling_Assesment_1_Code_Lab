b04b676f6a1ebb149c9111b9386dd45ae7539533
luvkapoipoi <165130641+luvkapoipoi@users.noreply.github.com>
2024-11-16 23:20:57 +0400

    Done
    Done the loops

--- 07.SomeCounting.py
+++ 07.SomeCounting.py
@@ -12,11 +12,41 @@
 
 # 0 to 50 in increments of 1
 print("Starting to count up from 0 to 50... ")
-for i in range(0, 51):
-    print (i)
+for int in range(0, 51): #This will start from 0 to 50
+    print (int)
+print("Done!")
 
 # 50 to 0 in decrements of 1
 print("Starting to count down from 50 to 0...")
-for i in range(51, 0):
-    print (i)
+for int in range(50, -1, -1): #This will start from 50 going down to 0
+    print (int)
+print("Done!")
 
+# This loop follows three arguments for the range feature: (x, y, z) 
+#   x= starting value
+#   y= ending value
+#   z= step value
+
+# 30 to 50 in increments of 1
+print("Starting to count up from 30 to 50...")
+for int in range(30, 51): #This starts at 30 going up to 50
+    print (int)
+print("Done!")
+
+# 50 to 10 in decrements of 2
+print("Starting to count down from 50 to 10...")
+for int in range(50, 9, -2): #This starts at 50 ending at 10
+    print(int)
+print("Done!")
+
+# Why 9 and not 10?
+# Placing 9 as an ending value makes the program print 10 as the last number.
+
+# 100 to 200 in increments of 5
+print("Starting to count up from 100 to 200 ")
+for int in range(100, 201, 5): #This starts from 100 to 200
+    print(int)
+print("Done!")
+
+# Explanation: From these loops, the range function is used with the for loop
+#              simplifying the proess of writing a for loop program
